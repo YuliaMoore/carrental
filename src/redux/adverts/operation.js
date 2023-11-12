d2c89ea9ec3431520d142924c9f99c02da02f883
YuliaMoore <115689321+YuliaMoore@users.noreply.github.com>
2023-11-12 22:48:09 +0200

    add jsonconfig

--- src/redux/adverts/operation.js
+++ src/redux/adverts/operation.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseUrl = "https://64bc43077b33a35a444727f9.mockapi.io";
+axios.defaults.baseUrl = "https://64bc43077b33a35a444727f9.mockapi.io/adverts";
 
 export const fetchCars = createAsyncThunk(
-  "adverts/fetchAll",
+  "advert/fetchAll",
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get("./advert");
+      const response = await axios.get("./adverts");
+      console.log(fetchCars);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
